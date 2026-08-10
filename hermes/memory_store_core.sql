05df57e9afca90dd4d6ad9bb233e1d3c74266ee9
Coordinate35 <Coordinate35@hust.edu.cn>
2026-08-11 03:01:30 +0800

    sync: 2026-08-11-03:01

--- hermes/memory_store_core.sql
+++ hermes/memory_store_core.sql
@@ -1,6 +1,6 @@
 -- Hermes Holographic Memory Export (text-only)
 -- Facts count: 7904
--- Exported: 2026-08-10T03:00:26.034998
+-- Exported: 2026-08-11T03:01:29.785638
 BEGIN TRANSACTION;
 
 CREATE TABLE entities (
